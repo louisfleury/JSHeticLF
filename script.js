97d70452b65f02328aed5fde8eacd87ee56e321e
louisfleury <louis.fleury@hetic.net>
2020-05-06 13:50:39 +0200

    fichtre quel engouement

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
-document.getElementById("p").innerHTML= "Epoustouflan";
-document.getElementById("p").style.color="red";
-document.getElementById("p").style.fontFamily="comic sans MS";
+document.addEventListener("DOMContentLoaded", function () {
+  class Model {
+    constructor() {
+      this.pages = [
+        {
+          title: "Homepage",
+          url: "/",
+          background: "red",
+        },
+        {
+          title: "Contact",
+          url: "/contact",
+          background: "green",
+        },
+      ];
+    }
+
+    getPageByUrl(url) {
+      return this.pages.find((page) => page.url === url);
+    }
+  }
+
+  class View {
+    constructor(container) {
+      this.container = document.querySelector(`.${container}`);
+    }
+
+    changeTitle(word) {
+      this.container.innerHTML = word;
+    }
+
+    changeBackground(color) {
+      this.container.style.background = color;
+    }
+  }
+
+  function controller() {
+    const data = new Model();
+    const currentPage = data.getPageByUrl("/contact");
+
+    const page = new View("container");
+    page.changeTitle(currentPage.title);
+    page.changeBackground(currentPage.background);
+  }
+
+  controller();
+});
